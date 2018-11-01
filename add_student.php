c28289fbdf6c935f113625af69841e9bd5262fab
CerchiaCorneliu <44298913+CerchiaCorneliu@users.noreply.github.com>
2018-11-01 10:39:44 +0200

    Add files via upload

---
+++ add_student.php
@@ -0,0 +1,34 @@
+<?php 
+
+	// 	Get values from form
+	$firstName = $_POST['first_name'];
+	$lastName = $_POST['last_name'];
+	$address = $_POST['address'];
+	$age = $_POST['age'];
+
+	// echo $firstName." ".$lastName." ".$address." ".$age;
+
+	// Prepare variabiles for db connection
+	$serverName = "localhost";
+	$userName = "root";
+	$password = "";
+	$dbName = "admin";
+
+	// Create connection
+	$conn = new mysqli($serverName,$userName,$password,$dbName);
+
+	// Check connection
+		if($conn->connect_error) {
+			die("Connection failed".$conn->connect_error);
+		}
+
+	//Prepare and bind statement
+	$stmt = $conn->prepare("INSERT INTO students (firstname,lastname,address,age) VALUES (?,?,?,?)");
+	$stmt->bind_param("sssi", $firstName, $lastName, $address, $age);
+	$stmt->execute();
+
+	mysqli_stmt_close($stmt);
+	mysqli_close($conn);
+	header('Location:students.php');
+
+?>
